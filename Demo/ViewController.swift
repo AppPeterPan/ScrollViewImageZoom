0d9c535db6f28bd82ad2c223297372849112e9e5
peterpan <apppeterpan@gmail.com>
2020-11-30 18:46:20 +0800

    update

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        updateZoomSizeFor(size: view.bounds.size)
+    }
 
+    func updateZoomSizeFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        if scale < 1 {
+            scrollView.minimumZoomScale = scale
+            scrollView.zoomScale = scale
+        }
+        let maxScale = max(widthScale, heightScale)
+        if maxScale > 1 {
+            scrollView.maximumZoomScale = maxScale
+        }
+    }
+    
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let inset = (scrollView.bounds.height - imageView.bounds.height * scrollView.zoomScale) / 2
+        scrollView.contentInset = .init(top: max(inset, 0), left: 0, bottom: 0, right: 0)
+        print(scrollView.zoomScale)
+    }
+}
